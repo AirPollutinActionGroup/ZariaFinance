7b6b4e132a26e60020392d826c215e830dfcbe12
itashutoshsingh <itashutoshsingh@gmail.com>
2026-07-02 23:32:18 +0530

    backend work UserRegister,UserLogin,Role worke compliteite

--- backend/finance/src/main/java/com/ngo/finance/userRegister/entity/UserLoginRepository.java
+++ backend/finance/src/main/java/com/ngo/finance/userRegister/entity/UserLoginRepository.java
@@ -1,9 +1,7 @@
 package com.ngo.finance.userRegister.entity;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface UserLoginRepository extends JpaRepository<UserRegister, Long> {
 
 }
