6a06dfa042148c50b34ccda0b66d0701593d016e
itashutoshsingh <itashutoshsingh@gmail.com>
2026-07-29 01:40:52 +0530

    new update

--- backend/finance/src/main/java/com/ngo/finance/donor/enums/ScheduleFrequency.java
+++ backend/finance/src/main/java/com/ngo/finance/donor/enums/ScheduleFrequency.java
@@ -1,7 +1,6 @@
 package com.ngo.finance.donor.enums;
 
 public enum ScheduleFrequency {
-    ONE_TIME("One Time"),
     MONTHLY("Monthly"),
     QUARTERLY("Quarterly"),
     HALF_YEARLY("Half Yearly"),
